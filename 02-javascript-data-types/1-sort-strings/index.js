7b57570543367a5f3e9fcb2fefbe01ed266aaab1
Andrey Demchenko <andrey9744@yandex.ru>
2023-07-18 17:26:39 +0300

    добавил корректировки к первой задаче второго модуля

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,13 +5,5 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = "asc") {
-    const newArr = [...arr];
-    return newArr.sort((a, b) => {
-      if (param === 'asc') {
-        return a.localeCompare(b, ["ru-RU", "en-EN"], {caseFirst: 'upper'});
-      }
-       else if (param === 'desc') {
-        return b.localeCompare(a, ["ru-RU", "en-EN"], {caseFirst: 'upper'});
-      };
-    });
+    return [...arr].sort((a, b) => ((param === 'asc')? a.localeCompare(b, ["ru-RU", "en-EN"], {caseFirst: 'upper'}): b.localeCompare(a, ["ru-RU", "en-EN"], {caseFirst: 'upper'})));
   };
