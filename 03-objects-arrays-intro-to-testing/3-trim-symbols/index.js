6f6274c05329e59931e9fce7f1de8571fa299d85
Andrey Demchenko <andrey9744@yandex.ru>
2023-07-29 23:56:49 +0300

    Третий Модуль, объекты и массивы 3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
-}
+  if (size === 0) {
+  	return '';
+  };
+  if (!size) {
+  	return string;
+  };
+  let count = 0;
+  let result = '';
+  for (const char of string) {
+    if (result[result.length - 1] === char) {
+      if (count < size) {
+        result += char;
+        count++;
+      };
+    } else {
+      count = 1;
+      result += char;
+    };
+  };
+  return result;
+};
